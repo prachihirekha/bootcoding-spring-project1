c04a46ef1166283d6e25a245f8c46dfeb2904482
prachihirekha <prachihirekhan32@gmail.com>
2023-08-20 15:12:57 +0530

    projects

---
+++ patient-app/src/main/java/com/patient/application/controller/InvoicePatientController.java
@@ -0,0 +1,25 @@
+package com.patient.application.controller;
+
+import com.patient.application.model.InvoiceModePatient;
+import com.patient.application.model.Patient;
+import com.patient.application.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patientinvoice")
+public class InvoicePatientController {
+    @Autowired
+    private PatientService patientService;
+    @PostMapping("/patient")
+    public String registor(@RequestBody InvoiceModePatient patient){
+        // System.out.println(patient);
+        patientService.singlePatient(patient);
+        return "Success";
+    }
+
+
+}
