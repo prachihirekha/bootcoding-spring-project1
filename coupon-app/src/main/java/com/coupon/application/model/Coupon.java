3159e786eeb2fdbd14b44ebd5ed0508256716d0f
prachihirekha <prachihirekhan32@gmail.com>
2023-08-24 08:56:56 +0530

    couponService

--- coupon-app/src/main/java/com/coupon/application/model/Coupon.java
+++ coupon-app/src/main/java/com/coupon/application/model/Coupon.java
@@ -1,13 +1,37 @@
 package com.coupon.application.model;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 @Builder
+@Entity
+@Table(name="coupon_application")
 public class Coupon {
-    private String id;
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column(name ="coupon_Type")
     private String type;
+    @Column(name ="valid_date")
     private int validFor;
+    @Column(name="title")
+    private String title;
+    private String coupon_code;
+    private  String description;
+    private String category;
+
+    private double discount;
+    private Date create_date;
+
+    private  String create_by;
+
+    private boolean is_active;
+    private String status;
+
+
 
 }
